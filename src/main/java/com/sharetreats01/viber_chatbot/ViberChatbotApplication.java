1bc77c526fdf18a8e6e9e5649e25a32d682f5333
dlwlgh9412 <dlwlgh9412@gmail.com>
2023-06-16 03:32:02 +0900

    init

---
+++ src/main/java/com/sharetreats01/viber_chatbot/ViberChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.sharetreats01.viber_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViberChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViberChatbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sharetreats01/viber_chatbot/ViberChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharetreats01.viber_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViberChatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
