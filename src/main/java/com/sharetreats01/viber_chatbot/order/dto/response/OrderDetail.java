4a14f2f074a7e2decfd28fc9a91839944a0c040e
OolongTea620 <rlafls9596@gmail.com>
2023-06-22 16:18:20 +0900

    Order Dto 추가

---
+++ src/main/java/com/sharetreats01/viber_chatbot/order/dto/response/OrderDetail.java
@@ -0,0 +1,45 @@
+package com.sharetreats01.viber_chatbot.order.dto.response;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * ORDER API 호출 시, 받는 코드
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDetail {
+
+    private Long id;
+
+    private Long userId;
+
+    private String viberId;
+
+    private Long brandId;
+
+    private String brandName;
+
+    private String productId;
+
+    private Long productName;
+
+    private String giverPhone;
+
+    private String giverName;
+
+    private String receiverPhone;
+
+    private String receiverName;
+
+    private String couponCode;
+
+    private Integer price;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+}
