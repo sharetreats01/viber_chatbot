2dd0f284971b6755daacecf39155be922d0c7606
dlwlgh9412 <dlwlgh9412@gmail.com>
2023-06-23 14:05:25 +0900

    flyway 이슈로 인한 placeholder prefix 수정

---
+++ src/main/resources-dev/db/migration/V5__modify_test_template_value.sql
@@ -0,0 +1,7 @@
+update tb_message_template
+set template = '환영합니다! {name}님 쉐어트리츠 챗봇입니다.'
+where id = 1;
+
+update tb_message_template
+set template = 'Welcome! {name} I''m Share Treats Chatbot.'
+where id = 2;
