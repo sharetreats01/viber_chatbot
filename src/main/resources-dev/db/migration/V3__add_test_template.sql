6b54fa06d029ac8bf080a52360fa87e012786acb
dlwlgh9412 <dlwlgh9412@gmail.com>
2023-06-23 13:53:49 +0900

    message_template 초기화값 수정

--- src/main/resources-dev/db/migration/V3__add_test_template.sql
+++ src/main/resources-dev/db/migration/V3__add_test_template.sql
@@ -1,5 +1,5 @@
 alter table tb_message_template modify column created_at timestamp default now();
 alter table tb_message_template modify column modified_at timestamp default now();
 
-insert into tb_message_template (type, template, language, created_at, modified_at, version) VALUES ('WELCOME', '환영합니다! ${name}님 쉐어트리츠 챗봇입니다.', 'ko', default, default, 1);
-insert into tb_message_template (type, template, language, created_at, modified_at, version) VALUES ('WELCOME', 'Welcome! ${name} I''m Share Treats Chatbot.', 'en', default, default, 1);
+insert into tb_message_template (type, template, language, created_at, modified_at, version) VALUES ('WELCOME', '환영합니다! $${name}님 쉐어트리츠 챗봇입니다.', 'ko', default, default, 1);
+insert into tb_message_template (type, template, language, created_at, modified_at, version) VALUES ('WELCOME', 'Welcome! $${name} I''m Share Treats Chatbot.', 'en', default, default, 1);
