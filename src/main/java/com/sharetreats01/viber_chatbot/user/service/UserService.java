68d3790b153bbca1a264088991310e6ec1e35618
dlwlgh9412 <dlwlgh9412@gmail.com>
2023-06-26 16:25:54 +0900

    no message

--- src/main/java/com/sharetreats01/viber_chatbot/user/service/UserService.java
+++ src/main/java/com/sharetreats01/viber_chatbot/user/service/UserService.java
@@ -1,7 +1,6 @@
 package com.sharetreats01.viber_chatbot.user.service;
 
 import com.sharetreats01.viber_chatbot.interaction.dto.callback.parameter.User;
-import com.sharetreats01.viber_chatbot.user.entity.UserEntity;
 
 public interface UserService {
     void subscribe(User user);
