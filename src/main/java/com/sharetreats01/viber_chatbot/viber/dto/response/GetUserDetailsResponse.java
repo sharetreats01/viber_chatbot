c4334a1735ef29275108cbf4b2d3283a1fdaa583
dlwlgh9412 <dlwlgh9412@gmail.com>
2023-06-27 13:48:34 +0900

    생성자 변경으로 인한 테스트코드 수정

--- src/main/java/com/sharetreats01/viber_chatbot/viber/dto/response/GetUserDetailsResponse.java
+++ src/main/java/com/sharetreats01/viber_chatbot/viber/dto/response/GetUserDetailsResponse.java
@@ -2,24 +2,23 @@ package com.sharetreats01.viber_chatbot.viber.dto.response;
 
 import com.sharetreats01.viber_chatbot.user.dto.User;
 import com.sharetreats01.viber_chatbot.viber.enums.Status;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-public class GetUserDetailsResponse extends ViberResponse {
-    private final Long messageToken;
-    private final User user;
-
-    public GetUserDetailsResponse(Status status, String statusMessage, Long messageToken, User user) {
-        super(status, statusMessage);
-        this.messageToken = messageToken;
-        this.user = user;
-    }
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class GetUserDetailsResponse {
+    private Status status;
+    private String statusMessage;
+    private Long messageToken;
+    private User user;
 
     @Override
     public String toString() {
         return "GetUserDetailsResponse{" +
-                "status=" + super.getStatus() +
-                ", statusMessage=" + super.getStatusMessage() +
+                "status=" + status +
+                ", statusMessage='" + statusMessage + '\'' +
                 ", messageToken=" + messageToken +
                 ", user=" + user +
                 '}';
--- src/test/java/com/sharetreats01/viber_chatbot/user/service/UserServiceImplTest.java
+++ src/test/java/com/sharetreats01/viber_chatbot/user/service/UserServiceImplTest.java
@@ -7,6 +7,7 @@ import com.sharetreats01.viber_chatbot.user.repository.UserRepository;
 import com.sharetreats01.viber_chatbot.viber.client.ViberWebClient;
 import com.sharetreats01.viber_chatbot.viber.dto.GetUserDetailsResponseFactory;
 import com.sharetreats01.viber_chatbot.viber.dto.request.GetUserDetailsRequest;
+import com.sharetreats01.viber_chatbot.viber.dto.response.GetUserDetailsResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -41,7 +42,7 @@ class UserServiceImplTest {
     @Test
     public void 유저_구독_테스트() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         User user = UserDtoFactory.createUserDto("01234567890A=", "John McClane", "http://avatar.example.com", "UK", "en", 1);
-        com.sharetreats01.viber_chatbot.viber.dto.response.GetUserDetailsResponse response = GetUserDetailsResponseFactory.create();
+        GetUserDetailsResponse response = GetUserDetailsResponseFactory.create();
         when(userRepository.findById(any(String.class))).thenReturn(Optional.empty());
         when(viberWebClient.getUserDetails(any(GetUserDetailsRequest.class))).thenReturn(response);
         when(userRepository.save(any(UserEntity.class))).thenReturn(userEntity);
