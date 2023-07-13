fcb3aa4919d2f7152df016ac5e18dbe2160def0d
dlwlgh9412 <dlwlgh9412@gmail.com>
2023-07-13 20:12:21 +0900

    fix: AbstractMessageCreator getReceiverId수정
    SubscribedRequest 체크 로직 추가

--- src/main/java/com/sharetreats01/viber_chatbot/dto/callback/request/SubscribedRequest.java
+++ src/main/java/com/sharetreats01/viber_chatbot/dto/callback/request/SubscribedRequest.java
@@ -1,6 +1,5 @@
 package com.sharetreats01.viber_chatbot.dto.callback.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sharetreats01.viber_chatbot.dto.callback.request.property.User;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -9,6 +8,5 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SubscribedRequest extends MessageRequest{
-    @JsonProperty("user")
-    private User sender;
+    private User user;
 }
