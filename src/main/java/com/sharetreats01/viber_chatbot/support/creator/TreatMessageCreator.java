cfe8b8b5253dd79b5f19f0926a9728c0f8e06d1e
OolongTea620 <rlafls9596@gmail.com>
2023-07-18 03:53:10 +0900

    dev rebase 작업

--- src/main/java/com/sharetreats01/viber_chatbot/support/creator/TreatMessageCreator.java
+++ src/main/java/com/sharetreats01/viber_chatbot/support/creator/TreatMessageCreator.java
@@ -1,9 +1,7 @@
 package com.sharetreats01.viber_chatbot.support.creator;
 
 import com.sharetreats01.viber_chatbot.dto.callback.request.MessageRequest;
-import com.sharetreats01.viber_chatbot.dto.callback.request.property.Sender;
 import com.sharetreats01.viber_chatbot.infra.viber.dto.request.SendMessageRequest;
-import com.sharetreats01.viber_chatbot.order.dto.response.OrderSuccessResponse;
 
 import java.util.List;
 
